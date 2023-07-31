3a514a269e20191277914b629e7faa23599150a9
Fahad <205fahadmohammad@gmail.com>
2023-07-31 19:18:21 +0600

    login and signup added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOTEZYoiafg14L7XS0Y8rklenMmmnNops",
+  authDomain: "modern-desktop-cd566.firebaseapp.com",
+  projectId: "modern-desktop-cd566",
+  storageBucket: "modern-desktop-cd566.appspot.com",
+  messagingSenderId: "806435282991",
+  appId: "1:806435282991:web:92a6cfc4402d3ac590a6da",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,32 +1,43 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import { signIn } from "next-auth/react";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
+import {
+  useAuthState,
+  useCreateUserWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import Link from "next/link";
 import auth from "@/firebase/firebase.auth";
+import Image from "next/image";
 
-const LoginPage = () => {
-  const { register, handleSubmit, reset } = useForm();
+const SignUpPage = () => {
+  const [user, loading] = useAuthState(auth);
+  console.log(user);
 
-  const [signInWithEmailAndPassword, user, loading] =
-    useSignInWithEmailAndPassword(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
 
-  const onSubmit = (data) => {
-    signInWithEmailAndPassword(data.Email, data.Password);
+  const [createUserWithEmailAndPassword, eUser, eLoading, eError] =
+    useCreateUserWithEmailAndPassword(auth);
 
+  const onSubmit = async (data) => {
+    await createUserWithEmailAndPassword(data.Email, data.Password);
+    console.log(data);
     reset();
   };
 
-  if (loading) {
+  if (loading || eLoading) {
     return <p>Loading...</p>;
   }
+
   return (
-    <section className="pb-3">
-      <div className="bg-blueGray-50">
-        <div className="w-full lg:w-4/12 px-4 mx-auto">
+    <div className="pb-5">
+      <section className="bg-blueGray-50">
+        <div className="w-full lg:w-4/12 px-4 mx-auto pt-6">
           <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-xl rounded-lg bg-blueGray-200 border-0">
-            <div className="rounded-t mb-0 px-6 pb-6">
+            <div className="rounded-t mb-0 px-6 py-6">
               <div className="text-center mb-3">
                 <h6 className="text-blueGray-500 text-sm font-bold">
                   Sign in with
@@ -34,17 +45,14 @@ const LoginPage = () => {
               </div>
               <div className="btn-wrapper text-center">
                 <button
-                  onClick={() =>
-                    signIn("github", { callbackUrl: "http://localhost:3000" })
-                  }
                   className="bg-white active:bg-blueGray-50 text-blueGray-700  px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
                   type="button"
                 >
                   <Image
                     alt="..."
+                    className=" mr-1"
                     width={20}
                     height={20}
-                    className="mr-1"
                     src="https://demos.creative-tim.com/notus-js/assets/img/github.svg"
                   />
                   Github
@@ -57,7 +65,7 @@ const LoginPage = () => {
                     alt="..."
                     width={20}
                     height={20}
-                    className="mr-1"
+                    className=" mr-1"
                     src="https://demos.creative-tim.com/notus-js/assets/img/google.svg"
                   />
                   Google{" "}
@@ -75,10 +83,22 @@ const LoginPage = () => {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
                     Email
                   </label>
                   <input
-                    name="email"
                     type="email"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     placeholder="Email"
@@ -93,11 +113,23 @@ const LoginPage = () => {
                     Password
                   </label>
                   <input
-                    name="password"
                     type="password"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     placeholder="Password"
                     {...register("password")}
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="Confirm Password"
                   />
                 </div>
                 <div>
@@ -114,20 +146,13 @@ const LoginPage = () => {
                 </div>
                 <div className="text-center mt-6">
                   <button
-                    className=" text-black active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                    className="bg-blueGray-800 text-black active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                     type="submit"
                   >
                     {" "}
-                    LOGIN{" "}
+                    Sign Up{" "}
                   </button>
                 </div>
-                <div className="text-center mt-3">
-                  New Member?{" "}
-                  <Link className="text-blue-800" href="/signup">
-                    Please SignUp
-                  </Link>
-                </div>
-
                 <button className="text-center mx-auto mt-3 w-full">
                   <Link href="/" className="decoration-transparent text-black">
                     Back To Home
@@ -137,9 +162,9 @@ const LoginPage = () => {
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
-export default LoginPage;
+export default SignUpPage;
